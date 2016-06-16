3fbb64054c0892234a49d9eab0dc1d77e647da46
moshe.apelbaum@gmail.com <rdjue6h9>
2016-06-16 20:03:39 +0300

    current location works :)

--- app/app.component.js
+++ app/app.component.js
@@ -11,11 +11,27 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require("@angular/core");
 // import {Grid} from "./app.co";
 var Location = (function () {
-    function Location() {
+    function Location(row, col) {
     }
     return Location;
 }());
 exports.Location = Location;
+(function (CellState) {
+    CellState[CellState["visited"] = 0] = "visited";
+    CellState[CellState["unvisited"] = 1] = "unvisited";
+})(exports.CellState || (exports.CellState = {}));
+var CellState = exports.CellState;
+(function (CellIcon) {
+    CellIcon[CellIcon["unvisited"] = 0] = "unvisited";
+    CellIcon[CellIcon["visited_no_bomb"] = 1] = "visited_no_bomb";
+    CellIcon[CellIcon["visited_with_bomb"] = 2] = "visited_with_bomb";
+})(exports.CellIcon || (exports.CellIcon = {}));
+var CellIcon = exports.CellIcon;
+//
+// @Component({
+//     selector: 'cell',
+//     template: `<td>blabla</td>`
+// })
 var Cell = (function () {
     function Cell(_hasBomb, _state) {
         this._hasBomb = _hasBomb;
@@ -38,21 +54,22 @@ var Cell = (function () {
         enumerable: true,
         configurable: true
     });
-    Cell = __decorate([
-        core_1.Component({
-            selector: 'cell',
-            templateUrl: "\n    <div *ngIf=\"state = CellState.visited\">\n        V\n    </div>\n    <div *ngIf=\"state = CellState.unvisited\">\n        U\n    </div>\n    \n    \n"
-        }), 
-        __metadata('design:paramtypes', [Boolean, Number])
-    ], Cell);
+    Cell.prototype.getCellIconEnum = function () {
+        if (this._state == CellState.unvisited) {
+            return CellIcon.unvisited;
+        }
+        else if (this._state == CellState.visited) {
+            if (this._hasBomb) {
+                return CellIcon.visited_with_bomb;
+            }
+            else {
+                return CellIcon.visited_no_bomb;
+            }
+        }
+    };
     return Cell;
 }());
 exports.Cell = Cell;
-(function (CellState) {
-    CellState[CellState["visited"] = 0] = "visited";
-    CellState[CellState["unvisited"] = 1] = "unvisited";
-})(exports.CellState || (exports.CellState = {}));
-var CellState = exports.CellState;
 (function (Direction) {
     Direction[Direction["UP"] = 0] = "UP";
     Direction[Direction["RIGHT"] = 1] = "RIGHT";
@@ -99,6 +116,8 @@ var Grid = (function () {
             } while (isExist);
             cell.hasBomb = true;
         }
+        this.currentCell = this.board[0][0];
+        this.currentLocation = new Location(0, 0);
     };
     Grid.prototype.getNewLocation = function (direction) {
         return null;
@@ -111,7 +130,8 @@ var Grid = (function () {
     Grid = __decorate([
         core_1.Component({
             selector: 'grid',
-            template: "\n<table>\n    <tr *ngFor=\"let row of board\">\n        <td *ngFor=\"let col of row\">{{col.hasBomb}}</td>\n    </tr>\n</table>    \n"
+            template: "\n<table>\n    <tr *ngFor=\"let row of board\">\n        <td *ngFor=\"let cell of row\"> \n            <div [ngSwitch]=\"cell.getCellIconEnum()\">\n                <img *ngSwitchWhen=\"0\" src=\"./app/images/unknown.png\" alt=\"unknown\" height=\"42\" width=\"42\" style=\"position: relative; top: 0; left: 0;\" class=\"{{(cell == currentCell) ? 'saturate':''}}\" >\n                <img *ngSwitchWhen=\"1\" src=\"./app/images/visited_no_bomb.png\" alt=\"unknown\" height=\"42\" width=\"42\" style=\"position: relative; top: 0; left: 0;\">\n                <img *ngSwitchWhen=\"2\" src=\"./app/images/visited_with_bomb.png\" alt=\"unknown\" height=\"42\" width=\"42\" style=\"position: relative; top: 0; left: 0;\">                                \n            </div>\n            \n        </td>\n    </tr>\n</table>    \n",
+            styles: ["\n\n.saturate {-webkit-filter: saturate(3); filter: saturate(3);}\n.grayscale {-webkit-filter: grayscale(100%); filter: grayscale(100%);}\n.contrast {-webkit-filter: contrast(160%); filter: contrast(160%);}\n.brightness {-webkit-filter: brightness(0.25); filter: brightness(0.25);}\n.blur {-webkit-filter: blur(3px); filter: blur(3px);}\n.invert {-webkit-filter: invert(100%); filter: invert(100%);}\n.sepia {-webkit-filter: sepia(100%); filter: sepia(100%);}\n.huerotate {-webkit-filter: hue-rotate(180deg); filter: hue-rotate(180deg);}\n.rss.opacity {-webkit-filter: opacity(50%); filter: opacity(50%);}\n"]
         }), 
         __metadata('design:paramtypes', [])
     ], Grid);
@@ -149,7 +169,7 @@ var App = (function () {
         core_1.Component({
             selector: 'my-app',
             directives: [Grid],
-            template: "\n<div><grid></grid></div>\n"
+            template: "\n<div><grid></grid></div>\n<br><br>\n"
         }), 
         __metadata('design:paramtypes', [])
     ], App);
--- app/app.component.ts
+++ app/app.component.ts
@@ -2,24 +2,25 @@ import {Component, Input} from "@angular/core";
 // import {Grid} from "./app.co";
 
 export class Location {
-    row:number;
-    col:number;
+    constructor(row:number,
+                col:number) {
+    }
+
 }
-
-@Component({
-    selector: 'cell',
-
-    templateUrl: `
-    <div *ngIf="state = CellState.visited">
-        V
-    </div>
-    <div *ngIf="state = CellState.unvisited">
-        U
-    </div>
-    
-    
-`
-})
+export enum CellState {
+    visited,
+    unvisited,
+}
+export enum CellIcon{
+    unvisited,
+    visited_no_bomb,
+    visited_with_bomb
+}
+//
+// @Component({
+//     selector: 'cell',
+//     template: `<td>blabla</td>`
+// })
 
 export class Cell {
     constructor(private _hasBomb:boolean,
@@ -38,11 +39,23 @@ export class Cell {
         this._hasBomb = val;
     }
 
+    getCellIconEnum():CellIcon {
+        if (this._state == CellState.unvisited) {
+            return CellIcon.unvisited;
+        }
+        else if (this._state == CellState.visited) {
+            if (this._hasBomb) {
+                return CellIcon.visited_with_bomb;
+            }
+            else {
+                return CellIcon.visited_no_bomb;
+            }
+        }
+
+    }
+
 }
-export enum CellState {
-    visited,
-    unvisited,
-}
+
 
 export enum Direction{
     UP, RIGHT, DOWN, LEFT
@@ -53,10 +66,29 @@ export enum Direction{
     template: `
 <table>
     <tr *ngFor="let row of board">
-        <td *ngFor="let col of row">{{col.hasBomb}}</td>
+        <td *ngFor="let cell of row"> 
+            <div [ngSwitch]="cell.getCellIconEnum()">
+                <img *ngSwitchWhen="0" src="./app/images/unknown.png" alt="unknown" height="42" width="42" style="position: relative; top: 0; left: 0;" class="{{(cell == currentCell) ? 'saturate':''}}" >
+                <img *ngSwitchWhen="1" src="./app/images/visited_no_bomb.png" alt="unknown" height="42" width="42" style="position: relative; top: 0; left: 0;">
+                <img *ngSwitchWhen="2" src="./app/images/visited_with_bomb.png" alt="unknown" height="42" width="42" style="position: relative; top: 0; left: 0;">                                
+            </div>
+            
+        </td>
     </tr>
 </table>    
-`
+`,
+    styles: [`
+
+.saturate {-webkit-filter: saturate(3); filter: saturate(3);}
+.grayscale {-webkit-filter: grayscale(100%); filter: grayscale(100%);}
+.contrast {-webkit-filter: contrast(160%); filter: contrast(160%);}
+.brightness {-webkit-filter: brightness(0.25); filter: brightness(0.25);}
+.blur {-webkit-filter: blur(3px); filter: blur(3px);}
+.invert {-webkit-filter: invert(100%); filter: invert(100%);}
+.sepia {-webkit-filter: sepia(100%); filter: sepia(100%);}
+.huerotate {-webkit-filter: hue-rotate(180deg); filter: hue-rotate(180deg);}
+.rss.opacity {-webkit-filter: opacity(50%); filter: opacity(50%);}
+`]
 })
 export class Grid {
     currentCell:Cell;
@@ -65,7 +97,7 @@ export class Grid {
     cellsInPath:number = 0;
     totalRows:number = 10;
     totalCols:number = 10;
-    board:Cell[][]=[];
+    board:Cell[][] = [];
 
     ngOnInit() {
         this.plantBombs();
@@ -78,7 +110,7 @@ export class Grid {
     private plantBombs() {
         // mark all cells as clean from bombs
         for (let i = 0; i < this.totalRows; ++i) {
-            this.board[i]=[];
+            this.board[i] = [];
             for (let j = 0; j < this.totalCols; ++j) {
                 this.board[i][j] = new Cell(false, CellState.unvisited);
             }
@@ -98,9 +130,11 @@ export class Grid {
                     isExist = false;
                 }
             } while (isExist);
-            cell.hasBomb=true;
+            cell.hasBomb = true;
 
         }
+        this.currentCell = this.board[0][0];
+        this.currentLocation = new Location(0, 0);
 
     }
 
@@ -141,6 +175,7 @@ export class ScoreBoard {
     directives: [Grid],
     template: `
 <div><grid></grid></div>
+<br><br>
 `
 })
 export class App {
